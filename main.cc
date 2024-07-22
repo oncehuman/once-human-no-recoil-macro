8a8d7a905cd76160656a52ea1f792942bee333ad
oncehuman <altintuoro@gmail.com>
2024-07-23 01:10:05 +0500

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,12 @@
+# step 1
+step_1 = goto("discord link (discord.gg/5MS29Mxczw)")
+
+# step 2
+step_2 = watch("join the discord")
+
+# step 3
+step_3 = full_information("for further information about this check our YT channel https://www.youtube.com/@little_edge")
+
+# step 4
+step_4 = contact("contact us in our discord as guided in videos in our channel")
+
